dfdd49007321b677f13da29d2b6cb748f8ea701b
lmcnatt <85642387+lucymcnatt@users.noreply.github.com>
2024-10-16 17:22:07 -0400

    set global eval variable to false
    trying knitr_meta

--- convert_rmd_file.py
+++ convert_rmd_file.py
@@ -17,11 +17,11 @@ def to_safe_html(stream: bytes) -> str:
     binary_data = stream.read()
     raw_rmd = binary_data.decode('ascii')
 
-    # Remove executable code blocks
-    safe_rmd = _sanitize_rmd(raw_rmd)
+    # # Remove executable code blocks
+    # safe_rmd = _sanitize_rmd(raw_rmd)
 
     # Convert to HTML
-    raw_html = _to_html(safe_rmd)
+    raw_html = _to_html(raw_rmd)
 
     # Remove unsafe HTML
     safe_html = sanitize_body(raw_html)
@@ -29,29 +29,21 @@ def to_safe_html(stream: bytes) -> str:
     return safe_html
 
 
-def _sanitize_rmd(data: str) -> bytes:
-    """Strip code blocks (ex ```{bash}) from .rmd string.
-
-    When rendering, kitr (https://rmarkdown.rstudio.com/authoring_quick_tour.html#Rendering_Output) executes all code in these blocks.
-    Running arbitrary user code on the Calhoun server would leave us exposed to attack.
-    For details, see https://docs.google.com/document/d/1aNCOKitTJH-GEkBSR4i-x91O0OQCZ8ZYa3feXtkja94/edit#heading=h.rvpr6zoz0jem
-
-    Returns:
-        bytestring of .rmd without executable code blocks.
-    """
-    lines = data.split('\n')
-    sanitized_file = []
-    for line in lines:
-        if line.startswith('```'):
-            sanitized_line = '```'
-        elif line.startswith('`'):
-            sanitized_line = '`' + line[2:]
-        else:
-            sanitized_line = line
-        sanitized_file.append(sanitized_line)
-
-    file = '\n'.join(sanitized_file)
-    return file.encode('ascii')
+# def _sanitize_rmd(data: str) -> bytes:
+#     """Strip code blocks (ex ```{bash}) from .rmd string.
+#
+#     When rendering, knitr (https://rmarkdown.rstudio.com/authoring_quick_tour.html#Rendering_Output) executes all code in these blocks.
+#     Running arbitrary user code on the Calhoun server would leave us exposed to attack.
+#     For details, see https://docs.google.com/document/d/1aNCOKitTJH-GEkBSR4i-x91O0OQCZ8ZYa3feXtkja94/edit#heading=h.rvpr6zoz0jem
+#
+#     Returns:
+#         bytestring of .rmd without executable code blocks.
+#     """
+#
+#     dont_run_code_block = '`r knitr::opts_chunk$set(eval = FALSE)`'
+#     file = dont_run_code_block + data
+#
+#     return file.encode('ascii')
 
 
 def _to_html(data: bytes) -> str:
@@ -70,7 +62,7 @@ def _to_html(data: bytes) -> str:
         # Call R rmarkdown package from python.
         # See https://cran.r-project.org/web/packages/rmarkdown/index.html
         rmd = importr('rmarkdown')
-        rendered_html = rmd.render(in_file.name)
+        rendered_html = rmd.render(in_file.name, knitr_meta={'eval': False})
         out_path = rendered_html[0]
 
         try:
